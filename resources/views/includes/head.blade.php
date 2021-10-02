789ed1aa0501564f1b59bad62b10cd0bc7ce62ed
SaskiaZwarts <71122362+SaskiaZwarts@users.noreply.github.com>
2021-10-02 15:47:15 +0200

    Footer social update
    Css van footer social links veranderd

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -5,7 +5,7 @@
 <meta name="keywords" content="userguide, user's guide, guide, manual, manual, owners manual, referenceguide, quickguide, service manual, free, pdf, download"/>
 <meta name="language" content="en">
 <meta name="google-site-verification" content="Fdb3h6i9VSbRysXMwdteA4E2tZnedlygCcIyJBADqKo" />
-
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css">
 <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet"/>
 
